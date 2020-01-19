0166319848a95c6c80b42aac22120df910610c17
JaspreetSinghGill <jaspreetsg06@gmail.com>
2020-01-19 20:43:30 +0530

    ADDED: validated the optional part i.e. country code with whole email pattern

--- EmailPattern.sh
+++ EmailPattern.sh
@@ -15,7 +15,8 @@ checkPattern () {
 
 emailPatternMain () {
 	askUserInput;
-	pattern="^[a-zA-Z]{1}[a-zA-Z0-9]*([-.+_]{1}[a-zA-Z0-9]+)?[@]{1}[a-zA-Z0-9]+[.]{1}[a-z]{2,4}$"; #VALIDATED EMAIL GROUP 4 OPTIONAL PART
+	#VALIDATED THE OPTIONAL PART COUNTRY CODE WITH WHOLE EMAIL PATTERN.
+	pattern="^[a-zA-Z]{1}[a-zA-Z0-9]*([-.+_]{1}[a-zA-Z0-9]+)?[@]{1}[a-zA-Z0-9]+[.]{1}[a-z]{2,4}([.][a-z]{2})?$";
 	checkPattern;
 }
 
